8b758a88619adfe06316155c4a5a275e041127da
Parham <parham.fazel@spatialvison.com.au>
2015-04-07 09:02:25 +1000

    Removed extra files

--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('farmBuildNutrientCalculator.version', [
-  'farmBuildNutrientCalculator.version.interpolate-filter',
-  'farmBuildNutrientCalculator.version.version-directive'
-])
-
-.value('version', '0.1');
