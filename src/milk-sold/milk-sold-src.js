6855234e4ac7e18857cd232322cda0e1114bbb70
Parham <parham.fazel@spatialvison.com.au>
2015-04-08 17:14:03 +1000

    Increased version

--- src/milk-sold/milk-sold-src.js
+++ src/milk-sold/milk-sold-src.js
@@ -1,9 +1,9 @@
 /**
-* @since 1.2.3
+* @since 0.0.1
 * @copyright 2015 Spatial Vision, Inc. http://spatialvision.com.au
 * @license The MIT License
 * @author Spatial Vision
-* @version 1.2.3
+* @version 0.0.1
 */
 
 'use strict';
@@ -15,13 +15,14 @@
 angular.module('farmBuildNutrientCalculator', ['farmBuildNutrientCalculator.version'])
 
 	.factory('MilkSold', function () {
+
 		var exports = {};
 
 		/**
 		 * Calculates nutrient from milk sold, input values are in percentage
-		 * @param {number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
-		 * @param {number} milkProteinPercentage - Percentage of milk protein
-		 * @param {number} milkFatPercentage - Percentage of milk fat
+		 * @param {!number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
+		 * @param {!number} milkProteinPercentage - Percentage of milk protein
+		 * @param {!number} milkFatPercentage - Percentage of milk fat
 		 * @returns {object} milk nutrient data
 		 * @public
 		 */
@@ -29,28 +30,28 @@ angular.module('farmBuildNutrientCalculator', ['farmBuildNutrientCalculator.vers
 			var milkProteinInKg, milkFatInKg;
 
 			if (!milkSoldPerYearInLitre || !milkProteinPercentage || !milkFatPercentage) {
-				return;
+				return undefined;
 			}
 
-			if(!isNumber(milkSoldPerYearInLitre) || !isNumber(milkProteinPercentage) || !isNumber(milkFatPercentage)){
-				return;
+			if(!_isNumber(milkSoldPerYearInLitre) || !_isNumber(milkProteinPercentage) || !_isNumber(milkFatPercentage)){
+				return undefined;
 			}
 
 			if(milkProteinPercentage + milkFatPercentage > 100){
-				return;
+				return undefined;
 			}
 
-			milkProteinInKg = percentageToKg(milkProteinPercentage, milkSoldPerYearInLitre);
-			milkFatInKg = percentageToKg(milkFatPercentage, milkSoldPerYearInLitre);
-			return nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage);
+			milkProteinInKg = _percentageToKg(milkProteinPercentage, milkSoldPerYearInLitre);
+			milkFatInKg = _percentageToKg(milkFatPercentage, milkSoldPerYearInLitre);
+			return _nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage);
 
 		};
 
 		/**
 		 * Calculates nutrient from milk sold, input values are in Kg
-		 * @param {number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
-		 * @param {number} milkFatInKg - Quantity of milk fat in Kilograms
-		 * @param {number} milkProteinInKg - Quantity of milk protein in Kilograms
+		 * @param {!number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
+		 * @param {!number} milkFatInKg - Quantity of milk fat in Kilograms
+		 * @param {!number} milkProteinInKg - Quantity of milk protein in Kilograms
 		 * @returns {object} milk nutrient data
 		 * @public
 		 */
@@ -58,34 +59,34 @@ angular.module('farmBuildNutrientCalculator', ['farmBuildNutrientCalculator.vers
 			var milkProteinPercentage, milkFatPercentage;
 
 			if (!milkSoldPerYearInLitre || !milkFatInKg || !milkProteinInKg) {
-				return;
+				return undefined;
 			}
 
-			if(!isNumber(milkSoldPerYearInLitre) || !isNumber(milkProteinInKg) || !isNumber(milkFatInKg)){
-				return;
+			if(!_isNumber(milkSoldPerYearInLitre) || !_isNumber(milkProteinInKg) || !_isNumber(milkFatInKg)){
+				return undefined;
 			}
 
 			if(milkFatInKg + milkProteinInKg > milkSoldPerYearInLitre){
-				return;
+				return undefined;
 			}
 
-			milkFatPercentage = kgToPercentage(milkFatInKg, milkSoldPerYearInLitre);
-			milkProteinPercentage = kgToPercentage(milkProteinInKg, milkSoldPerYearInLitre);
-			return nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage);
+			milkFatPercentage = _kgToPercentage(milkFatInKg, milkSoldPerYearInLitre);
+			milkProteinPercentage = _kgToPercentage(milkProteinInKg, milkSoldPerYearInLitre);
+			return _nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage);
 
 		};
 
 		/**
 		 * Returns nutrient data of milk
-		 * @param {number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
-		 * @param {number} milkFatInKg - Quantity of milk fat in Kilograms
-		 * @param {number} milkProteinInKg - Quantity of milk protein in Kilograms.
-		 * @param {number} milkProteinPercentage - Percentage of milk protein
-		 * @param {number} milkFatPercentage - Percentage of milk fat
+		 * @param {!number} milkSoldPerYearInLitre - Quantity of milk sold in a year in litre
+		 * @param {!number} milkFatInKg - Quantity of milk fat in Kilograms
+		 * @param {!number} milkProteinInKg - Quantity of milk protein in Kilograms.
+		 * @param {!number} milkProteinPercentage - Percentage of milk protein
+		 * @param {!number} milkFatPercentage - Percentage of milk fat
 		 * @returns {object} milk nutrient data
 		 * @private
 		 */
-		function nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage) {
+		function _nutrientInMilkSold(milkSoldPerYearInLitre, milkFatInKg, milkProteinInKg, milkProteinPercentage, milkFatPercentage) {
 
 			var nitrogenPercentage = milkProteinPercentage / 6.33,
 				phosphorusPercentage = (0.0111 * milkFatPercentage + 0.05255),
@@ -115,33 +116,33 @@ angular.module('farmBuildNutrientCalculator', ['farmBuildNutrientCalculator.vers
 
 		/**
 		 * Returns nutrient value in percentage
-		 * @param {number} valueInKg - Quantity of milk protein in Kilograms
-		 * @param {number} totalInLitre - Percentage of milk protein
+		 * @param {!number} valueInKg - Quantity of milk protein in Kilograms
+		 * @param {!number} totalInLitre - Percentage of milk protein
 		 * @returns {number} nutrient value in percentage
 		 * @private
 		 */
-		function kgToPercentage(valueInKg, totalInLitre) {
+		function _kgToPercentage(valueInKg, totalInLitre) {
 			return (valueInKg / totalInLitre) * 100;
 		}
 
 		/**
 		 * Returns nutrient value in Kg
-		 * @param {number} valuePercentage - Quantity of milk protein in Kilograms
-		 * @param {number} totalInLitre - Percentage of milk protein
+		 * @param {!number} valuePercentage - Quantity of milk protein in Kilograms
+		 * @param {!number} totalInLitre - Percentage of milk protein
 		 * @returns {number} nutrient value in Kg
 		 * @private
 		 */
-		function percentageToKg(valuePercentage, totalInLitre) {
+		function _percentageToKg(valuePercentage, totalInLitre) {
 			return (valuePercentage * totalInLitre) / 100;
 		}
 
 		/**
-		 * Returns true if value is a number
-		 * @param {number} value - The value to be examined as a number
+		 * Returns true if value is a !number
+		 * @param {!number} value - The value to be examined as a number
 		 * @returns {boolean}
 		 * @private
 		 */
-		function isNumber(value) {
+		function _isNumber(value) {
 			return !isNaN(parseFloat(value)) && isFinite(value);
 		}
 
